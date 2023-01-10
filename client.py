263ce2bad85e99d70a8c76c044f1ebba81b1fa9b
Reem Khaled <reemkhaledaboushama@gmail.com>
2023-01-11 01:12:54 +0200

    client.py - create download file page, select from drop down menu file to download from server, stored in project's directory

--- client.py
+++ client.py
@@ -31,6 +31,10 @@ port = 6060
 
 
 
+
+
+
+
 def uploadeditems():
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("dark-blue")
@@ -44,19 +48,83 @@ def uploadeditems():
     label3 = ctk.CTkLabel(master=frame3, text="Uploaded files")
     label3.pack(pady=12, padx=10)
 
-
     with FTP(host) as ftp:
         ftp.connect(host=host, port=port)
         ftp.login(user=username_entry.get(), passwd=password_entry.get())
-        fileitems= ftp.nlst()
+        fileitems = ftp.nlst()
         for i in fileitems:
             print(i)
-            itemlabel = ctk.CTkLabel(master=frame3, text=i, width=480, height=30, corner_radius=10, fg_color="#1c4966")
+            itemlabel = ctk.CTkLabel(master=frame3, text=i, width=480, height=30, corner_radius=10, fg_color="#1f538d")
             itemlabel.place(relx=0.5, rely=0.5, anchor=ctk.W)
             itemlabel.pack(pady=5, padx=10)
 
     root.mainloop()
 
+
+def chooseGUI():
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("dark-blue")
+    root = ctk.CTk()
+    root.title("Upload or Download")
+    root.geometry("500x500")
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
+    label = ctk.CTkLabel(master=frame, text="upload or download")
+    label.pack(pady=12, padx=10)
+    download_button = ctk.CTkButton(master=frame, text="Download", command=lambda: filedownloadGUI())
+    download_button.pack(pady=12, padx=10)
+
+    upload_button = ctk.CTkButton(master=frame, text="Upload", command=lambda: fileuploadGUI())
+    upload_button.pack(pady=12, padx=10)
+
+
+    root.mainloop()
+
+def download(combox):
+    val = combox.get()
+    print(val)
+    with FTP(host) as ftp:
+        ftp.connect(host=host, port=port)
+        ftp.login(user=username_entry.get(), passwd=password_entry.get())
+        with open(val, "wb") as file:
+
+            ftp.retrbinary(f"RETR {val}", file.write)
+            # quit and close the connection
+        ftp.quit()
+
+
+
+
+
+def filedownloadGUI():
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("dark-blue")
+    root = ctk.CTk()
+    root.title("Download items")
+    root.geometry("500x500")
+    frame4 = ctk.CTkFrame(master=root)
+
+    label4 = ctk.CTkLabel(master=frame4, text="Download")
+    frame4.place(height=250, width=500)
+    frame4.pack(pady=20, padx=60, fill="both", expand=True)
+    label4.pack(pady=12, padx=10)
+
+    with FTP(host) as ftp:
+        ftp.connect(host=host, port=port)
+        ftp.login(user=username_entry.get(), passwd=password_entry.get())
+        fileitems = ftp.nlst()
+
+    combobox = ctk.CTkOptionMenu(master=frame4, values=fileitems)
+    combobox.pack(padx=20, pady=10)
+
+
+
+
+
+    download_button = ctk.CTkButton(master=frame4, text="Download", command=lambda: download(combobox))
+    download_button.pack(pady=12, padx=10)
+
+    root.mainloop()
 
 
 def upload():
@@ -79,30 +147,7 @@ def upload():
 
 
 def fileuploadGUI():
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("dark-blue")
-    root = ctk.CTk()
-    root.title("File Upload")
-    root.geometry("500x500")
-    frame2 = ctk.CTkFrame(master=root)
-    frame2.pack(pady=20, padx=60, fill="both", expand=True)
-    label = ctk.CTkLabel(master=frame2, text="Secure Upload")
-    label.pack(pady=12, padx=10)
-
-
-    # upload file btn
-    secureupld_button = ctk.CTkButton(master=frame2, text="Secure Upload", command=upload)
-    secureupld_button.pack(pady=12, padx=10)
-
-
-
-
-    root.mainloop()
-
-
-
-
-
+   upload()
 
 
 
@@ -117,7 +162,7 @@ def accessFTP():
         print(ftp.getwelcome())
         print(username_entry.get())
 
-        fileuploadGUI()
+        chooseGUI()
 
         return user, password
 
