640f8657da589234546b55b39f1c718a989ccaeb
Reem Khaled <reemkhaledaboushama@gmail.com>
2023-01-10 18:57:12 +0200

    client.py - added username and password entries, encapsulated GUI initialization in a function

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 import customtkinter as ctk
 
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
 
-root = ctk.CTk()
-root.geometry("500x500")
+def initGUI():
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("dark-blue")
 
-frame1 = ctk.CTkFrame(master=root)
-frame1.pack(pady=20, padx=60, fill="both", expand=True)
-label= ctk.CTkLabel(master=frame1, text="Login to FTP")
-label.pack(pady=12, padx=10)
+    root = ctk.CTk()
+    root.geometry("500x500")
 
-root.mainloop()
+    frame1 = ctk.CTkFrame(master=root)
+    frame1.pack(pady=20, padx=60, fill="both", expand=True)
+    label= ctk.CTkLabel(master=frame1, text="Login to FTP")
+    label.pack(pady=12, padx=10)
+
+    username_entry = ctk.CTkEntry(master=frame1, placeholder_text="Enter Username")
+    username_entry.pack(pady=12, padx=10)
+
+    password_entry = ctk.CTkEntry(master=frame1, placeholder_text="Enter Password", show="*")
+    password_entry.pack(pady=12, padx=10)
+
+    root.mainloop()
+
+
+initGUI()
